3938fed29597d1f38fe4f63dc361ec5679c28267
Neel Smith <dnsmith.neel@gmail.com>
2021-06-24 08:15:01 -0400

    Commit working tfidf script

--- scripts/tfidf.jl
+++ scripts/tfidf.jl
@@ -1,3 +1,5 @@
+# Walk through TF-IDF with julia TextAnalysis module
+# by manually construcing a toy example.
 using TextAnalysis
 s1 = "Four score and seven years ago."
 d1 = StringDocument(s1)
@@ -7,11 +9,13 @@ c = Corpus([d1,d2])
 update_inverse_index!(c)
 update_lexicon!(c)
 
+# Index, by term, of what documents the term appears in:
+term2docs = inverse_index(c)
+lex = lexicon(c)
+docterm = DocumentTermMatrix(c)
+tfidf = tf_idf(docterm)
 
-invidx = inverse_index(c)
-m = DocumentTermMatrix(c)
-tfidf = tf_idf(m)
-
+#=
 doc1row = dtv(c[1],lexicon(c))
 doc2row = dtv(c[2],lexicon(c))
 
@@ -19,36 +23,25 @@ doc2row = dtv(c[2],lexicon(c))
 
 println(doc1row)
 println(doc2row)
-println(keylist)
-println(c[2].text)
-keylist |> length
+println(lex.keys)
+=#
 
-function reverseidx(trm, ks)   
-    idx = nothing
-    #findfirst(isequal(trm), ks)
-    for i in 1:length(ks)
-        if isassigned(ks,i)
-            if ks[i] == trm
-                idx = i
-                println("FOUND TERM KEY $i")
-            end
-        end
-    end
-    idx
+# Find index of term within docterm matrix.
+function revidx(trm, docterm)
+    findfirst(t -> t == trm, docterm.terms)
 end
 
 
+
 function reportidf(term)
-    #termidx = findfirst(isequal(term), keylist)
-    termidx = reverseidx(term,lex.keys)
-    keylist[termidx]
+    termidx = revidx(term, docterm)
     for i in 1:length(c.documents)
         println(term," in document ",i, " has tf-idf ", tfidf[i, termidx])
     end
 end
 
-term = "seven"
-termidx = reverseidx(term,lex.keys)
+term = "ago"
+termidx = revidx(term,docterm)
 
 
 termidx
